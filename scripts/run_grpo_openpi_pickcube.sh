6d9593ae7be618bba50f7aa890809e4c7ce06e1a
Kaitlyn Wang <kaitwang@stanford.edu>
2026-03-10 15:47:07 -0700

    grpo openpi config

---
+++ scripts/run_grpo_openpi_pickcube.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --job-name=grpopi05
+#SBATCH --output=/juice5b/scr5b/kaitwang/cs234/RLinf/logs/grpopi05_%A.out
+#SBATCH --error=/juice5b/scr5b/kaitwang/cs234/RLinf/logs/grpopi05_%A.err
+#SBATCH --cpus-per-task=20
+#SBATCH --gres=gpu:4
+#SBATCH --constraint=48G
+#SBATCH --partition=jag-standard
+#SBATCH --account=nlp
+
+export TMPDIR=/juice5b/scr5b/kaitwang/tmp
+export RAY_TMPDIR=/juice5b/scr5b/kaitwang/tmp/ray
+export UV_CACHE_DIR=/juice5b/scr5b/kaitwang/.uv_cache
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+
+cd /juice5b/scr5b/kaitwang/cs234/RLinf
+source .venv/bin/activate
+export RAY_ADDRESS="local"
+bash examples/embodiment/run_embodiment.sh grpo_openpi_pi05_pickcube
